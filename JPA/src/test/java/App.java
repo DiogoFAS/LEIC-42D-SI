4aecb4d5386269ab60d018f1e8bff783fb2fe6c5
diogo guerra <A49149@alunos.isel.pt>
2023-06-12 20:20:58 +0100

    Optimisitic and pessimistic implementation

--- JPA/src/test/java/App.java
+++ JPA/src/test/java/App.java
@@ -7,27 +7,12 @@ public class App {
     protected interface ITest {
         void test();
     }
-
-    private static void teste8() throws Exception{
-        Scanner imp = new Scanner(System.in );
-        System.out.printf("Número de repetições para teste 8? ");
-        int nreps = imp.nextInt();
-        BLService_Tests srv = new BLService_Tests();
-        srv.teste8(nreps);
-    }
-
     public static void main(String[] args) {
         // Guerra Branch
         BLService_Tests srv = new BLService_Tests();
         ITest tests[] = new ITest[] {
-                () -> {try { srv.teste1(); } catch(Exception e) {}} ,
-                () -> {try { srv.teste2(); } catch(Exception e) {}} ,
-                () -> {try { srv.teste3(); } catch(Exception e) {}} ,
-                () -> {try { srv.teste4(); } catch(Exception e) {}} ,
-                () -> {try { srv.teste5(); } catch(Exception e) {}} ,
-                () -> {try { srv.teste6(); } catch(Exception e) {}} ,
-                () -> {try { srv.teste7(); } catch(Exception e) {}} ,
-                () -> {try { teste8(); } catch(Exception e) {}}
+            () -> {try { srv.teste1(); } catch(Exception e) {}} ,
+            () -> {try { srv.teste2(); } catch(Exception e) {}}
         };
 
         Scanner imp = new Scanner(System.in );
@@ -35,5 +20,6 @@ public class App {
         int option = imp.nextInt();
         if (option >= 1 && option <= tests.length)
             tests[--option].test();
+
     }
 }
--- JPA/src/test/java/business_logic/BLService_Tests.java
+++ JPA/src/test/java/business_logic/BLService_Tests.java
@@ -20,216 +20,70 @@ public class BLService_Tests {
     @Description("Test the manages conection and transactions")
     public void teste1() throws Exception {
         // ****************** Teste 1 ****************
-        try(DataScope scope = new DataScope()) {
-            EntityManager em = scope.getEntityManager();
-            CrachaId crachaKeys = new CrachaId();
-            //insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('TetrisRank1', 'Tetris', 1000, 'http:/XadrezRank1.jpg');
-            crachaKeys.setNome("TetrisRank1");
-            crachaKeys.setNomejogo("Tetris");
+        // Mudar nome da base de dados
+        Cracha c;
+        Cracha ent;
 
-            Cracha c = em.find(Cracha.class, crachaKeys);
-            if (c == null) throw new Exception("Count find cracha.");
-            System.out.println("teste1 success!");
-            scope.validateWork();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw e;
-        }
-    }
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SI");
+        EntityManager em = emf.createEntityManager();
 
-    @Description("Test the manages conection and transactions")
-    public void teste2() throws Exception {
-        // ****************** Teste 2 ****************
-
-        //Verificar início e fim de transações na BD
-        try(DataScope scope =  new DataScope()) {
-            EntityManager em = scope.getEntityManager();
+        try {
+            em.getTransaction().begin();
             CrachaId crachaKeys = new CrachaId();
             crachaKeys.setNome("TetrisRank1");
             crachaKeys.setNomejogo("Tetris");
-            //acquire unit of work
-            //Conta a = em.find(Conta.class, Long.valueOf(1111),LockModeType.PESSIMISTIC_WRITE);
+            c = em.find(Cracha.class, crachaKeys, LockModeType.OPTIMISTIC);
+            if (c == null)
+                throw new GameOnAppExc("cracha inexistente");
 
-            Cracha c = em.find(Cracha.class, crachaKeys);
-            c.setLimitedepontos((int) (c.getLimitedepontos() + (c.getLimitedepontos() * 0.2)));
-            scope.validateWork();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw e;
-        }
-    }
+            int observedValue = c.getLimitedepontos();
+            int newValue = (int) (c.getLimitedepontos() * 1.2);
 
-
-    @Description("Test the manages conection and transactions")
-    public void teste3() throws Exception {
-        // ****************** Teste 3 ****************
-
-        //Verificar início e fim de transações na BD
-        try(DataScope scope = new DataScope()) {
-            EntityManager em = scope.getEntityManager();
-
-            CrachaId crachaKeys = new CrachaId();
-            crachaKeys.setNome("TetrisRank1");
-            crachaKeys.setNomejogo("Tetris");
-
-            //acquire unit of work
-            //Conta a = em.find(Conta.class, Long.valueOf(1111),LockModeType.PESSIMISTIC_WRITE);
-            Cracha c = em.find(Cracha.class, crachaKeys);
-            c.setLimitedepontos((int) (c.getLimitedepontos() + (c.getLimitedepontos() * 0.2)));
-            em.flush();
-            scope.validateWork();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw e;
-        }
-    }
-
-    @Description("Test concurrency control")
-    public void teste4() throws Exception {
-        // ****************** Teste 4 ****************
-        //Verificar problemas de conssistência
-        try(DataScope scope = new DataScope()) {
-            EntityManager em = scope.getEntityManager();
-
-            CrachaId crachaKeys = new CrachaId();
-            crachaKeys.setNome("TetrisRank1");
-            crachaKeys.setNomejogo("Tetris");
-
-            Cracha c = em.find(Cracha.class, crachaKeys);
-
-            if (c.getLimitedepontos() >= 600) {
-                c.setLimitedepontos( (c.getLimitedepontos() - 600));
+            if (c.getLimitedepontos() == observedValue) {
+                c.setLimitedepontos(newValue);
                 em.getTransaction().commit();
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw e;
+            em.getTransaction().commit();
+
+        } catch (RollbackException|OptimisticLockException e) {
+            if (e.getCause() instanceof OptimisticLockException || e instanceof OptimisticLockException) {
+                if (e instanceof OptimisticLockException)
+                    ent = (Cracha) ((OptimisticLockException) e).getEntity(); // devolve null
+                else
+                    ent = (Cracha) ((OptimisticLockException) e.getCause()).getEntity(); // devolve null
+                System.out.printf("Erro de concorrência com o crachá: %d\n", ent.getId());
+                if (em.getTransaction().isActive())
+                    em.getTransaction().rollback();
+            } else throw e;
         }
     }
 
     @Description("Test pessimistic concurrency control")
-    public void teste5() throws Exception {
-        // ****************** Teste 5 ****************
+    public void teste2() throws Exception {
+        // ****************** Teste 2 ****************
 
-        //Controlo de concorrência pessimista
-         try(DataScope scope = new DataScope()) {
-            EntityManager em = scope.getEntityManager();
-
+        // Mudar nome da base de dados
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SI");
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
             CrachaId crachaKeys1 = new CrachaId();
             crachaKeys1.setNome("TetrisRank1");
             crachaKeys1.setNomejogo("Tetris");
 
-            Cracha c1 = em.find(Cracha.class, crachaKeys1, LockModeType.PESSIMISTIC_READ);
+            Cracha c1 = em.find(Cracha.class, crachaKeys1, LockModeType.PESSIMISTIC_FORCE_INCREMENT);
 
-            CrachaId crachaKeys2 = new CrachaId();
-            // insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('DamasRank1', 'Damas', 1000, 'http://DamasRank1.jpg');
-            crachaKeys2.setNome("SpaceInv");
-            crachaKeys2.setNomejogo("SpaceInvRank1");
+            int observedValue = c1.getLimitedepontos();
+            int newValue = (int) (c1.getLimitedepontos() * 1.2);
 
-            Cracha c2 = em.find(Cracha.class, crachaKeys2, LockModeType.PESSIMISTIC_WRITE);
+            if (c1.getLimitedepontos() == observedValue) {
+                c1.setLimitedepontos(newValue);
+                em.getTransaction().commit();
+            }
 
-            c1.setLimitedepontos((int) (c1.getLimitedepontos() + (c1.getLimitedepontos() * 0.2)));
-            em.flush();
-            scope.validateWork();
         } catch (Exception e) {
             System.out.println(e.getMessage());
             throw e;
         }
-    }
-
-    @Description("Test optimistic concurrency control")
-    public void teste6() throws Exception {
-        // ****************** Teste 6 ****************
-
-        //Controlo de concorrência otimista
-        // Para ser usado com @OptimisticLocking(cascade=true,type=OptimisticLockingType.CHANGED_COLUMNS) comentado em ContaOpt.java
-        //
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgres");
-        EntityManager em = emf.createEntityManager();
-        try {
-            em.getTransaction().begin();
-            Mapper<Cracha, CrachaId> map = new Mapper<>(Cracha.class, CrachaId.class);
-
-            CrachaId crachaKeys = new CrachaId();
-            crachaKeys.setNome("TetrisRank1");
-            crachaKeys.setNomejogo("Tetris");
-
-            Cracha c = em.find(Cracha.class, crachaKeys, LockModeType.OPTIMISTIC);
-
-            c.setLimitedepontos((int) (c.getLimitedepontos() + (c.getLimitedepontos() * 0.2)));
-            map.update(c);
-            em.flush();
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw e;
-        }
-    }
-
-    @Description("Test optimistic concurrency control")
-    public void teste7() throws Exception {
-        // ****************** Teste 7 ****************
-
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgres");
-        EntityManager em = emf.createEntityManager();
-
-        try {
-            em.getTransaction().begin();
-
-            CrachaId crachaKeys = new CrachaId();
-            crachaKeys.setNome("TetrisRank1");
-            crachaKeys.setNomejogo("Tetris");
-
-            Cracha c = em.find(Cracha.class, crachaKeys);
-
-            c.setLimitedepontos((int) (c.getLimitedepontos() * 1.2));
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw e;
-        } finally {
-            em.close();
-            emf.close();
-        }
-    }
-
-    @Description("Test optimistic concurrency control")
-    public void teste8(int nreps) throws Exception {
-        // ****************** Teste 8 ****************
-        Cracha c = null;
-        Cracha ent;
-
-        try(DataScope scope = new DataScope()) {
-            EntityManager em = scope.getEntityManager();
-            do {
-                try {
-                    --nreps;
-                    CrachaId crachaKeys = new CrachaId();
-                    crachaKeys.setNome("TetrisRank1");
-                    crachaKeys.setNomejogo("Tetris");
-                    c = em.find(Cracha.class, crachaKeys, LockModeType.OPTIMISTIC);
-                    if (c == null)
-                        throw new GameOnAppExc("cracha inexistente");
-
-                    c.setLimitedepontos((int) (c.getLimitedepontos() + (c.getLimitedepontos() * 0.2)));
-                    // experimentar com flush e com commit
-                    //em.flush();
-
-                    nreps = 0;
-                    scope.validateWork();
-                } catch (RollbackException | OptimisticLockException e) {
-                    if (e.getCause() instanceof OptimisticLockException || e instanceof OptimisticLockException) {
-                        if (em.getTransaction().isActive())
-                            em.getTransaction().rollback();
-                        if (nreps == 0)
-                            throw new GameOnAppExc("erro de concorrência");
-                    } else throw e;
-                }
-            } while (nreps > 0);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw e;
-        }
-
     }
 }
