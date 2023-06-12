490480ae1bda2634c693fc267f6902fe17ec8ec7
DiogoSantos <A48459@alunos.isel.pt>
2023-06-12 15:33:22 +0100

    insert table

--- dataTables/insertTable.sql
+++ dataTables/insertTable.sql
@@ -13,52 +13,52 @@ insert into Jogador (id, estado, username, email, nomeRegiao) values (5, 'Ativo'
 insert into Jogador (id, estado, username, email, nomeRegiao) values (6, 'Ativo', 'DaniloBoy', 'DaniloBoy@gmail.com', 'Chelas');
 
 --insert Jogo
-insert into Jogo (nome, id, URL) values ('Xadrez', 1, 'http:/Xadrez');
-insert into Jogo (nome, id, URL) values ('Damas', 2, 'http://Damas');
+insert into Jogo (nome, id, URL) values ('Tetris', 1, 'http://Tetris64');
+insert into Jogo (nome, id, URL) values ('Pacman', 2, 'http://Pacman');
 insert into Jogo (nome, id, URL) values ('Snake', 3, 'http://Snake');
-insert into Jogo (nome, id, URL) values ('BatalhaNaval', 4, 'http://BatalhaNaval');
+insert into Jogo (nome, id, URL) values ('SpaceInv', 4, 'http://SpaceInvaders');
 
 --insert Cracha
-insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('XadrezRank1', 'Xadrez', 1000, 'http:/XadrezRank1.jpg');
-insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('XadrezRank2', 'Xadrez', 500, 'http:/XadrezRank2.jpg');
-insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('XadrezRank3', 'Xadrez', 250, 'http:/XadrezRank3.jpg');
-insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('XadrezRank4', 'Xadrez', 100, 'http:/XadrezRank4.jpg');
+insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('TetrisRank1', 'Tetris', 1000, 'http://TetrisRank1.jpg');
+insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('TetrisRank2', 'Tetris', 500, 'http://TetrisRank2.jpg');
+insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('TetrisRank3', 'Tetris', 250, 'http://TetrisRank3.jpg');
+insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('TetrisRank4', 'Tetris', 100, 'http://TetrisRank4.jpg');
 
-insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('DamasRank1', 'Damas', 1000, 'http://DamasRank1.jpg');
-insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('DamasRank2', 'Damas', 500, 'http://DamasRank2.jpg');
-insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('DamasRank3', 'Damas', 250, 'http://DamasRank3.jpg');
-insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('DamasRank4', 'Damas', 100, 'http://DamasRank4.jpg');
+insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('PacmanRank1', 'Pacman', 1000, 'http://PacmanRank1.jpg');
+insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('PacmanRank2', 'Pacman', 500, 'http://PacmanRank2.jpg');
+insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('PacmanRank3', 'Pacman', 250, 'http://PacmanRank3.jpg');
+insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('PacmanRank4', 'Pacman', 100, 'http://PacmanRank4.jpg');
 
 insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('SnakeRank1', 'Snake', 1000, 'http://SnakeRank1.jpg');
 insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('SnakeRank2', 'Snake', 500, 'http://SnakeRank2.jpg');
 insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('SnakeRank3', 'Snake', 250, 'http://SnakeRank3.jpg');
 insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('SnakeRank4', 'Snake', 100, 'http://SnakeRank4.jpg');
 
-insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('BatalhaNavalRank1', 'BatalhaNaval', 1000, 'http://BatalhaNavalRank1.jpg');
-insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('BatalhaNavalRank2', 'BatalhaNaval', 500, 'http://BatalhaNavalRank2.jpg');
-insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('BatalhaNavalRank3', 'BatalhaNaval', 250, 'http://BatalhaNavalRank3.jpg');
-insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('BatalhaNavalRank4', 'BatalhaNaval', 100, 'http://BatalhaNavalRank4.jpg');
+insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('SpaceInvadersRank1', 'SpaceInv', 1000, 'http://SpaceInvadersRank1.jpg');
+insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('SpaceInvadersRank2', 'SpaceInv', 500, 'http://SpaceInvadersRank2.jpg');
+insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('SpaceInvadersRank3', 'SpaceInv', 250, 'http://SpaceInvadersRank3.jpg');
+insert into Cracha (nome, nomeJogo, limiteDePontos, URL) values ('SpaceInvadersRank4', 'SpaceInv', 100, 'http://SpaceInvadersRank4.jpg');
 
 --insert Partida
-insert into Partida (id, nomeJogo, dataFim) values (1, 'Xadrez', '2024-04-30 19:40:00');
-insert into Partida (id, nomeJogo, dataFim) values (2, 'Damas', '2024-04-30 19:40:00');
+insert into Partida (id, nomeJogo, dataFim) values (1, 'Tetris', '2024-04-30 19:40:00');
+insert into Partida (id, nomeJogo, dataFim) values (2, 'Pacman', '2024-04-30 19:40:00');
 insert into Partida (id, nomeJogo, dataFim) values (3, 'Snake', '2024-04-30 21:20:00');
-insert into Partida (id, nomeJogo, dataFim) values (4, 'BatalhaNaval', '2024-04-30 12:30:00');
-insert into Partida (id, nomeJogo, dataFim) values (5, 'BatalhaNaval', '2024-04-23 21:30:00');
+insert into Partida (id, nomeJogo, dataFim) values (4, 'SpaceInv', '2024-04-30 12:30:00');
+insert into Partida (id, nomeJogo, dataFim) values (5, 'SpaceInv', '2024-04-23 21:30:00');
 
 
 --insert MultiJogador
-insert into MultiJogador (idPartida, nomeJogo, estado, nomeRegiao) values (1, 'Xadrez', 'Terminada', 'Chelas');
-insert into MultiJogador (idPartida, nomeJogo, estado, nomeRegiao) values (4, 'BatalhaNaval', 'Terminada', 'Porto');
+insert into MultiJogador (idPartida, nomeJogo, estado, nomeRegiao) values (1, 'Tetris', 'Terminada', 'Chelas');
+insert into MultiJogador (idPartida, nomeJogo, estado, nomeRegiao) values (4, 'SpaceInv', 'Terminada', 'Porto');
 
 --insert Normal
-insert into Normal (idPartida, nomeJogo, dificuldade, idJogador, pontuacao) values (2, 'Damas', 4, 3, 350);
+insert into Normal (idPartida, nomeJogo, dificuldade, idJogador, pontuacao) values (2, 'Pacman', 4, 3, 350);
 insert into Normal (idPartida, nomeJogo, dificuldade, idJogador, pontuacao) values (3, 'Snake', 3, 2, 450);
-insert into Normal (idPartida, nomeJogo, dificuldade, idJogador, pontuacao) values (5, 'BatalhaNaval', 3, 3, 500);
+insert into Normal (idPartida, nomeJogo, dificuldade, idJogador, pontuacao) values (5, 'SpaceInv', 3, 3, 500);
 
 
 --insert Jogar
-insert into Jogar (idPartida, nomeJogo, idJogador, pontuacao) values (1, 'Xadrez', 1, 450);
-insert into Jogar (idPartida, nomeJogo, idJogador, pontuacao) values (1, 'Xadrez', 6, 500);
-insert into Jogar (idPartida, nomeJogo, idJogador, pontuacao) values (4, 'BatalhaNaval', 3, 700);
-insert into Jogar (idPartida, nomeJogo, idJogador, pontuacao) values (4, 'BatalhaNaval', 5, 650);
+insert into Jogar (idPartida, nomeJogo, idJogador, pontuacao) values (1, 'Tetris', 1, 450);
+insert into Jogar (idPartida, nomeJogo, idJogador, pontuacao) values (1, 'Tetris', 6, 500);
+insert into Jogar (idPartida, nomeJogo, idJogador, pontuacao) values (4, 'SpaceInv', 3, 700);
+insert into Jogar (idPartida, nomeJogo, idJogador, pontuacao) values (4, 'SpaceInv', 5, 650);
